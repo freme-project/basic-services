81bdc2493aa5bf6a1f95cdf1ce57a5a2c6f6bd84
jnehring <gmnoname@gmx.de>
2016-10-10 15:24:34 +0200

    #114

--- filters/cors-filter/src/main/java/eu/freme/bservices/filters/cors/CORSFilter.java
+++ filters/cors-filter/src/main/java/eu/freme/bservices/filters/cors/CORSFilter.java
@@ -28,7 +28,7 @@ public class CORSFilter implements Filter {
 		response.setHeader("Access-Control-Allow-Methods",
 				"POST, GET, OPTIONS, DELETE, PUT");
 		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Headers", "*");
+		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, content-type, x-auth-token, x-auth-username, x-auth-password");
 		chain.doFilter(req, res);
 	}
 
