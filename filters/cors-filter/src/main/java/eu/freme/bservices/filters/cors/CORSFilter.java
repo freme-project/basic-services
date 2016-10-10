7ac0c1df39fb0b5eb7c37ae04cc19bc640e364a6
jnehring <gmnoname@gmx.de>
2016-10-10 14:43:52 +0200

    #114

--- filters/cors-filter/src/main/java/eu/freme/bservices/filters/cors/CORSFilter.java
+++ filters/cors-filter/src/main/java/eu/freme/bservices/filters/cors/CORSFilter.java
@@ -26,7 +26,7 @@ public class CORSFilter implements Filter {
 		HttpServletResponse response = (HttpServletResponse) res;
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods",
-				"POST, GET, OPTIONS, DELETE");
+				"POST, GET, OPTIONS, DELETE, PUT");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "*");
 		chain.doFilter(req, res);
