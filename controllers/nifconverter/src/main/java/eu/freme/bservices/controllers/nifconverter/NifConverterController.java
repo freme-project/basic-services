366c87738c7cd952da4359be5d2dd1df0d60356f
bgrusdt <grusdt@uni-potsdam.de>
2016-12-08 17:30:13 +0100

    #108 allow TIKA to handle HTTP Post body

--- controllers/nifconverter/src/main/java/eu/freme/bservices/controllers/nifconverter/NifConverterController.java
+++ controllers/nifconverter/src/main/java/eu/freme/bservices/controllers/nifconverter/NifConverterController.java
@@ -17,31 +17,38 @@
  */
 package eu.freme.bservices.controllers.nifconverter;
 
-import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
-import eu.freme.bservices.internationalization.okapi.nif.converter.ConversionException;
-import eu.freme.common.conversion.SerializationFormatMapper;
-import eu.freme.common.conversion.rdf.RDFConstants;
-import eu.freme.common.exception.BadRequestException;
-import eu.freme.common.exception.FREMEHttpException;
-import eu.freme.common.exception.InternalServerErrorException;
-import eu.freme.common.rest.BaseRestController;
-import eu.freme.common.rest.NIFParameterSet;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.log4j.Logger;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.AutoDetectParser;
 import org.apache.tika.sax.BodyContentHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
-import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.util.Map;
+import com.hp.hpl.jena.rdf.model.Model;
+
+import eu.freme.common.exception.BadRequestException;
+import eu.freme.common.exception.FREMEHttpException;
+import eu.freme.common.rest.BaseRestController;
+import eu.freme.common.rest.NIFParameterSet;
 
 /**
  * This implements an anything to NIF converter.
@@ -54,88 +61,99 @@ import java.util.Map;
 @RestController
 public class NifConverterController extends BaseRestController{
 
-    Logger logger = Logger.getLogger(NifConverterController.class);
+	Logger logger = Logger.getLogger(NifConverterController.class);
 
-    @RequestMapping(value = "/toolbox/nif-converter", method = RequestMethod.POST)
-    public ResponseEntity<String> convert(
+	@Autowired HttpServletRequest request;
+
+	@RequestMapping(value = "/toolbox/nif-converter", method = RequestMethod.POST)
+	public ResponseEntity<String> convert(
 			HttpServletRequest request, 
-            @RequestHeader(value = "Accept") String acceptHeader,
-            @RequestHeader(value = "Content-Type") String contentTypeHeader,
-            @RequestBody(required = false) String postBody,
-            @RequestParam Map<String, String> allParams
-            ) {
+			@RequestHeader(value = "Accept") String acceptHeader,
+			@RequestHeader(value = "Content-Type") String contentTypeHeader,
+			@RequestBody(required = false) String postBody,
+			@RequestParam Map<String, String> allParams
+			) {
 
-    	if( ( allParams.containsKey("informat") && allParams.get("informat").equals("TIKAFile") ) ||
-    		(allParams.containsKey("i") && allParams.get("i").equals("TIKAFile"))	){
+		if( ( allParams.containsKey("informat") && allParams.get("informat").equals("TIKAFile") ) ||
+				(allParams.containsKey("i") && allParams.get("i").equals("TIKAFile"))	){
+
 			File inputFile = null;
-		    File tempDir = new File(System.getProperty("java.io.tmpdir"));
-		    MultipartFile file1 = null;
-		    if (request instanceof MultipartHttpServletRequest){
-		    	MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
-		    	file1 = multipartRequest.getFile("inputFile");
-		    	if(file1==null){
-		    		String msg = "There is no input file when TIKAFile informat has been specified";
-		            logger.error(msg);
-		            throw new BadRequestException(msg);
-		    	}
-		    	if (!file1.isEmpty()) {
-		    		try {
-		    			File tempFile = new File(tempDir + File.separator + file1.getOriginalFilename());
-		    			tempFile.createNewFile(); 
-		    			FileOutputStream fos = new FileOutputStream(tempFile); 
-		    			fos.write(file1.getBytes());
-		    			fos.close(); 
-		    			inputFile = tempFile;
-		    		} catch (Exception e) {
-			    		String msg = "There was a problem copying the inputfile";
-			            logger.error(msg);
-			            throw new BadRequestException(msg);
-		    		}
-		    	} else {
-		    		String msg = "The inputfile is empty";
-		            logger.error(msg);
-		            throw new BadRequestException(msg);
-		    	}
-		    }
-		    else{
-	    		String msg = "There is no file because it is not a multipart request";
-	            logger.error(msg);
-	            throw new BadRequestException(msg);
-		    }
-    		String text="";
-			try{
+			File tempDir = new File(System.getProperty("java.io.tmpdir"));
+			MultipartFile file1 = null;
+
+			if (request instanceof MultipartHttpServletRequest){
+				MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+				file1 = multipartRequest.getFile("inputFile");
+				if(file1==null){
+					String msg = "There is no input file when TIKAFile informat has been specified";
+					logger.error(msg);
+					throw new BadRequestException(msg);
+				}
+				if (!file1.isEmpty()) {
+					try {
+						File tempFile = new File(tempDir + File.separator + file1.getOriginalFilename());
+						tempFile.createNewFile(); 
+						FileOutputStream fos = new FileOutputStream(tempFile); 
+						fos.write(file1.getBytes());
+						fos.close(); 
+						inputFile = tempFile;
+					} catch (Exception e) {
+						String msg = "There was a problem copying the inputfile";
+						logger.error(msg);
+						throw new BadRequestException(msg);
+					}
+				} else {
+					String msg = "The inputfile is empty";
+					logger.error(msg);
+					throw new BadRequestException(msg);
+				}
+			}
+
+			// Read from file without MultipartRequest
+			else{
+				if(!allParams.containsKey("filename")){
+					String msg = "Please specify filename in the parameter 'filename'";
+					throw new BadRequestException(msg);
+				}
+
+				// read File from Body 		    	
+				inputFile = new File(tempDir + File.separator + allParams.get("filename"));
+
+			}
+
+			String text="";
+			try(InputStream stream = new FileInputStream(inputFile)){
 				AutoDetectParser parser = new AutoDetectParser();
 				BodyContentHandler handler = new BodyContentHandler();
 				Metadata metadata = new Metadata();
-				try (InputStream stream = new FileInputStream(inputFile)) {
-					parser.parse(stream, handler, metadata);
-					text = handler.toString();
-				}
+				parser.parse(stream, handler, metadata);
+				text = handler.toString();
 			}
 			catch(Exception e){
-	    		String msg = "There was a problem parsing the input file";
-	            logger.error(msg);
-	            throw new BadRequestException(msg);
+				String msg = "There was a problem parsing the input file";
+				logger.error(msg);
+				throw new BadRequestException(msg);
 			}
 			postBody = text;
-    		allParams.put("input", text);
-    		allParams.put("i", text);
-    		allParams.put("informat", "text");
-    		allParams.put("f", "text");
-    	}
+			allParams.put("input", text);
+			allParams.put("i", text);
+			allParams.put("informat", "text");
+			allParams.put("f", "text");
+		}
 
-    	NIFParameterSet nifParameters =  normalizeNif(postBody,
-                acceptHeader, contentTypeHeader, allParams, false);
-    	
-        try {
+		NIFParameterSet nifParameters =  normalizeNif(postBody,
+				acceptHeader, contentTypeHeader, allParams, false);
+
+		try {
 			Model model = getRestHelper().convertInputToRDFModel(nifParameters);
-            return createSuccessResponse(model, nifParameters.getOutformatString());
-        }catch (FREMEHttpException e){
-            logger.error("Error", e);
-            throw e;
-        } catch (Exception e) {
-            logger.error("Error", e);
-            throw new BadRequestException(e.getMessage());
-        }
-    }
+			return createSuccessResponse(model, nifParameters.getOutformatString());
+		}catch (FREMEHttpException e){
+			logger.error("Error", e);
+			throw e;
+		} catch (Exception e) {
+			logger.error("Error", e);
+			throw new BadRequestException(e.getMessage());
+		}
+	}
+
 }
